a901d6e054cc238d803d6313173b85acba4a52c1
lipengyu <lipengyu2006@gmail.com>
2022-02-21 15:53:38 +0800

    mobile first

--- js/content.js
+++ js/content.js
@@ -1,15 +1,7 @@
 (async function ($) {
-  const url_chapter = './html/chapter.html';
-  $('#chapter').load(url_chapter, function() {
-    console.log('have loaded chapter the content');
-  });
-  const url_screen = './html/screen.html';
-  $('#screen').load(url_screen, function() {
-    console.log('have loaded the screen of the content');
-    const url_chapter = './data/the_story_of_mankind/religious_warfare.html';
-    $('#article').load(url_chapter, function() {
-      console.log('have loaded the chapter of the content');
-    });
+  const url_chapter = '../data/the_story_of_mankind/religious_warfare.html';
+  $('#article').load(url_chapter, function() {
+    console.log('have loaded the chapter of the content');
   });
 })(jQuery);
 
