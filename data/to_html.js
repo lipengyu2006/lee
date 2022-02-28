c942b2a84c20e178141f11ee681973a72a4f078f
lipengyu <lipengyu2006@gmail.com>
2022-02-28 15:13:59 +0800

    the english revolution

--- data/to_html.js
+++ data/to_html.js
@@ -3,11 +3,15 @@
 const fs = require('fs');
 
 try {
-  const path = __dirname + '/the_story_of_mankind/';
-  const name = 'religious_warfare';
-  const chapter = '46';
-  const file_en = path + chapter + '_en_' + name;
-  const file_zh = path + chapter + '_zh_' + name;
+  const name = process.argv[2] || '';
+  const numb = process.argv[3] || 0;
+
+  if (!name || !numb) return;
+
+  const book = 'the_story_of_mankind';
+  const path = __dirname + '/' + book + '/';
+  const file_en = path + numb + '_en_' + name;
+  const file_zh = path + numb + '_zh_' + name;
   const data_en = fs.readFileSync(file_en, 'UTF-8');
   const data_zh = fs.readFileSync(file_zh, 'UTF-8');
 
@@ -15,14 +19,27 @@ try {
   const lines_zh = data_zh.split(/\r?\n/);
 
   const sections = [];
-  lines_en.forEach((line_en, i) => {
-    sections.push(
-    `<div class="section">
-      <p>${line_en}</p>
-      <p>${lines_zh[i]}</p>
-    </div>`
-    );
-  });
+  let i=0, j=0;
+  while (i<lines_en.length) {
+    const sentence = lines_en[i++];
+    if (/^img_/.test(sentence)) {
+      const des = lines_en[i++];
+      sections.push(
+      `<div class="plate">
+        <img src="../data/${ book }/${ sentence }.png">
+        <p>${ des }</p>
+      </div>`
+      );
+    } else {
+      const line_zh = lines_zh[j++];
+      sections.push(
+      `<div class="section">
+        <p>${ sentence }</p>
+        <p>${ line_zh }</p>
+      </div>`
+      );
+    }
+  }
 
   const out_file = path + name + '.html';
   fs.writeFileSync(out_file, sections.join(''));
